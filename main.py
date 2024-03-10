98b1e2d4978f82ca3f549a939d4c68dc976c3c3c
Keith Jung <60eokk@gmail.com>
2024-03-10 18:48:11 -0500

    added start, enddate

--- main.py
+++ main.py
@@ -1,33 +1,25 @@
 
 
-
 import requests
 
 api_key = '1ffP4T6741HAX9gA7xVs2XkY6aYX79wH'
 keyword = 'election'
+begin_date = '20200101'  # Example start date: January 1, 2020
+end_date = '20201231'    # Example end date: December 31, 2020
 
-# Constructing the URL for the API request
-url = f'https://api.nytimes.com/svc/search/v2/articlesearch.json?q={keyword}&api-key={api_key}'
+# Constructing the URL with date range
+url = f'https://api.nytimes.com/svc/search/v2/articlesearch.json?q={keyword}&begin_date={begin_date}&end_date={end_date}&api-key={api_key}'
 
-# Making the GET request to the New York Times Article Search API
 response = requests.get(url)
-# response = requests.get("https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=1ffP4T6741HAX9gA7xVs2XkY6aYX79wH")
 
-
-print("Status Code:", response.status_code)
-print("Response:", response.text)
-print("hi")
-
-
-# # Checking if the request was successful
-# if response.status_code == 200:
-#     # Convert the response to JSON
-#     articles = response.json()
+if response.status_code == 200:
+    articles = response.json()
     
-#     # Example: Print out the headline of the first article
-#     first_article_headline = articles['response']['docs'][0]['headline']['main']
-#     print(f"First article headline: {first_article_headline}")
-# else:
-#     print("Failed to fetch articles")
-
-
+    # Iterate over articles in the response
+    for doc in articles['response']['docs']:
+        headline = doc['headline']['main']
+        url = doc['web_url']
+        print(f"Title: {headline}\nURL: {url}\n")
+else:
+    print(f"Failed to fetch articles. Status Code: {response.status_code}")
+    print("Response:", response.text)
