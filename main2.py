1ebf9d5240a409a7945df8017cb89a5c023a02d1
Keith Jung <60eokk@gmail.com>
2024-07-19 11:25:03 -0500

    main2: def fetch_article

--- main2.py
+++ main2.py
@@ -4,6 +4,20 @@
 
 
 # 1. data collection (collect articles from past)
+# fetch and load articles --> will later be stored in csv/excel/sql
+def fetch_articles(apikey, keyword, from_date, to_date, page_size):
+    print(f"Fetching articles for '{keyword}' from {from_date} to {to_date}!")
+    base_url = "https://content.guardianapis.com/search"
+    params = {
+        'api_key': apikey,
+        'q': keyword,
+        'page_size': page_size,
+        'from-date': from_date,
+        'to-date': to_date,
+        'show-fields': body
+    }
+
+
 # 2. data processing (clean, process data)
 # 3. use nlp models to extract features (sentiment scores, etc)
 # 4. model training 
