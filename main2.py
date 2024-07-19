35accfa640802558f50e07e139be254c8a4efecb
Keith Jung <60eokk@gmail.com>
2024-07-19 11:05:24 -0500

    big outline on main2 file

--- main2.py
+++ main2.py
@@ -1,3 +1,10 @@
 # Approach to implement HuggingFace's NLP Models
 # In order to so: Will do by starting over, and taking functions from main.py
 
+
+
+# 1. data collection (collect articles from past)
+# 2. data processing (clean, process data)
+# 3. use nlp models to extract features (sentiment scores, etc)
+# 4. model training 
+# 5. prediction
