b0e01505996b4a2d6041a14ecee9530eb53b99db
Keith Jung <60eokk@gmail.com>
2024-07-21 15:36:24 -0400

    printing results to better analyze and data clean

--- main2.py
+++ main2.py
@@ -1,21 +1,47 @@
 # Approach to implement HuggingFace's NLP Models
 # In order to so: Will do by starting over, and taking functions from main.py
 
+import requests
+# from theguardian import theguardian_content
+
 
 
 # 1. data collection (collect articles from past)
 # fetch and load articles --> will later be stored in csv/excel/sql
 def fetch_articles(apikey, keyword, from_date, to_date, page_size):
     print(f"Fetching articles for '{keyword}' from {from_date} to {to_date}!")
-    base_url = "https://content.guardianapis.com/search"
+    url = "https://content.guardianapis.com/search"
     params = {
-        'api_key': apikey,
+        'api-key': apikey,
         'q': keyword,
-        'page_size': page_size,
+        'page-size': page_size, 
         'from-date': from_date,
         'to-date': to_date,
-        'show-fields': body
+        'show-fields': 'body'
     }
+
+    response = requests.get(url, params = params)
+
+    if response.status_code == 200:
+        print(response.json())
+    else:
+        print("error")
+
+
+def main():
+    apikey = '2ce72283-ccba-4b1a-92da-2f702366b61c'
+
+    # test case
+    keyword, from_date, to_date, page_size = "Trump", '2020-01-01', '2020-12-31', '2'
+    fetch_articles(apikey, keyword, from_date, to_date, page_size)
+
+
+if __name__ == "__main__":
+    main()
+
+#
+
+
 
 
 # 2. data processing (clean, process data)
